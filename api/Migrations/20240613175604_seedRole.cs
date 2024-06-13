ddb74f8f726d35488d583d108a56fdd23fefbc06
Sandrine Malanga <sandrinem@clearstar.net>
2024-06-13 21:07:49 +0300

    JWT, Register

---
+++ api/Migrations/20240613175604_seedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "b5abe420-ee2b-4111-9149-cbd26d2f81c9", null, "User", "USER" },
+                    { "dc11ecd1-48a3-4e57-b48a-7775a00a7026", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b5abe420-ee2b-4111-9149-cbd26d2f81c9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dc11ecd1-48a3-4e57-b48a-7775a00a7026");
+        }
+    }
+}
